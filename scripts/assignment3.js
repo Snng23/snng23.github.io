0f6d9d6c542d8106586db8123d66eb7a6d31bc77
Son Nguyen <95108441+Snng23@users.noreply.github.com>
2026-02-17 22:02:04 -0800

    Add visualization for question 1 topic 1

--- scripts/assignment3.js
+++ scripts/assignment3.js
@@ -1,3 +1,25 @@
 // DATA URL:
 const dataWide = "data/assignment3/videogames_wide.csv"; 
 const dataLong = "data/assignment3/videogames_long.csv";
+
+
+//
+vegaEmbed("t1q1", {
+    $schema: "https://vega.github.io/schema/vega-lite/v5.json",
+    description: "Global sales by genre.",
+    data: { url: dataWide },
+    mark: bar,
+    encoding: {
+        x: {
+            field: "Genre", 
+            type: "nominal", 
+            title: "Genre",
+        },
+
+        y: {
+            field: "Global_Sales",
+            type: "quantitative",
+            title: "Total Global Sales"
+        },
+    }
+}, { actions: false });
