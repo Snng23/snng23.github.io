cacc2ff50ab5b3af770a483fe7e5222e452afe61
Son Nguyen <95108441+Snng23@users.noreply.github.com>
2026-02-18 14:07:27 -0800

    add visualization for question 2 topic 1

--- scripts/assignment3.js
+++ scripts/assignment3.js
@@ -28,3 +28,29 @@ vegaEmbed("#t1q1", {
         },
     }
 }, { actions: false });
+
+
+vegaEmbed("#t1q2", {
+    $schema: "https://vega.github.io/schema/vega-lite/v5.json",
+    title: "Global Sales by Platform",
+    description: "Global sales by platform.",
+    width: "container",
+    height: 360,
+    data: { url: dataWide },
+    mark: "bar",
+    encoding: {
+        x: {
+            field: "Global_Sales",
+            type: "quantitative",
+            aggregate: "sum",
+            title: "Total Global Sales"
+        },
+
+        y: {
+            field: "Platform", 
+            type: "nominal", 
+            sort: "-x",
+            title: "Platform",
+        },
+    }
+}, { actions: false });
