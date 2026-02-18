e1e3489859b7b8a19c47848c926982cbdc22f2a0
Son Nguyen <95108441+Snng23@users.noreply.github.com>
2026-02-18 15:11:10 -0800

    Add graph for question 1 topic 2

--- scripts/assignment3.js
+++ scripts/assignment3.js
@@ -3,7 +3,8 @@ const dataWide = "data/assignment3/videogames_wide.csv";
 const dataLong = "data/assignment3/videogames_long.csv";
 
 
-//
+
+//VISUALIZATION 1 GRAPHS:
 vegaEmbed("#t1q1", {
     $schema: "https://vega.github.io/schema/vega-lite/v5.json",
     title: "Global Sales by Genre",
@@ -52,5 +53,36 @@ vegaEmbed("#t1q2", {
             sort: "-x",
             title: "Platform",
         },
+    }
+}, { actions: false });
+
+
+
+//VISUALIZATION 2 GRAPHS:
+vegaEmbed("#t2q1", {
+    $schema: "https://vega.github.io/schema/vega-lite/v5.json",
+    title: "Global Sales Over Time by Genre",
+    description: "Global sales over time by genre.",
+    width: "container",
+    height: 600,
+    data: { url: dataWide },
+    mark: "line",
+    encoding: {
+        x: {
+            field: "Year", 
+            type: "temporal", 
+            title: "Year",
+        },
+
+        y: {
+            field: "Global_Sales",
+            type: "quantitative",
+            aggregate: "sum",
+            title: "Total Global Sales"
+        },
+
+        color: {
+            field: "Genre"
+        }
     }
 }, { actions: false });
