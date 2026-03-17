51d2281efb3803e266325dff152c45de1c417f87
Son Nguyen <95108441+Snng23@users.noreply.github.com>
2026-03-16 20:47:54 -0700

    Update drawVis() function to draws every circles stored in circles list

--- scripts/d3.js
+++ scripts/d3.js
@@ -1,9 +1,14 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
-let svg;
-
 const width = 800;
 const height = 600;
+
+const maxCircles = 10;  // Maximum number of circles allowed
+let circles = []; // array of circles on the canvas, in form of { id, x, y }
+let nextId = 1; //Id for counting the number of circles
+
+let svg;
+
 
 async function prepareVis() {
   svg = d3.select("#d3")
@@ -12,18 +17,23 @@ async function prepareVis() {
   .attr("height", height)
 }
 
-// async function drawVis() {
-//   circle = svg
-//     .append("circle")
-//     .attr("r", 15)
-//     .attr("fill", "black")
-//     .attr("cx", 55)
-//     .attr("cy", 25)
-//     .on("click", playAnimation);
-// }
+
+//Draw all circles in the list of circles
+async function drawVis() {
+  svg
+    .selectAll("circle")
+    .data(circles, (d) => d.id)
+    .join("circle")
+    .attr("r", 15) // All circles will have radius of 15
+    .attr("fill", "#FF0000") // All circles will be in red
+    .attr("cx", (d) => d.x)
+    .attr("cy", (d) => d.y);
+}
+
 
 async function runApp() {
   await prepareVis();
+  await drawVis();
 }
 
 runApp();
